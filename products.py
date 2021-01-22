759a08e89bee3e351104b60ceab35b7c55d52e2a
Richard Specter Liu <richardliu0502@gmail.com>
2021-01-22 11:32:45 +0800

    add header

--- products.py
+++ products.py
@@ -9,3 +9,12 @@ print(products)
 
 for p in products:
 	print('Products:', p[0], 'Preis:', p[1])
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('product, price\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
+with open('products.txt', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
